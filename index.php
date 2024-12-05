46c30fb090ecc37845f2e4abcb3076128f1a66d7
nvolodin <nikolay.volodin.dev@gmail.com>
2024-12-05 00:28:42 -0500

    Init commit. Simple DFS.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'src/Graph.php';
+
+$vertices = [
+  'Toronto',
+  'Ottawa',
+  'Kingston',
+  'Montreal',
+  'Quebec City',
+  'Boston',
+  'New York City',
+  'Philadelphia',
+];
+$edges = [
+  ['Toronto', 'Ottawa'],
+  ['Ottawa', 'Montreal'],
+  ['Ottawa', 'Kingston'],
+  ['Kingston', 'Ottawa'],
+  ['Montreal', 'Quebec City'],
+  ['Boston', 'New York City'],
+];
+$graph = new \App\Graph($vertices, $edges);
+
+print "Largest Connected Components: " . $graph->countLargestConnectedComponents();
+print PHP_EOL;
